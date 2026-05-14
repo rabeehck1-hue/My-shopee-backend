bf836bed6d4a59a769343217562bfe8c332b3807
Muhammed rabeeh <muhammedrabeehck1@gmail.com>
2026-05-14 12:41:06 +0530

    env update

--- server.js
+++ server.js
@@ -157,6 +157,6 @@ app.listen(5000, () => {
 //mogodb connection
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/ecommerce")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
